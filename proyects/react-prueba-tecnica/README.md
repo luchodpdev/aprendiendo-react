f16fa719cb799f911cde7e7329d0c7c722b8438e
Luciano De Pasquale <luchodepasquale@gmail.com>
2024-08-12 18:33:52 +0200

    Prueba Tecnica React Completada

--- proyects/react-prueba-tecnica/README.md
+++ proyects/react-prueba-tecnica/README.md
@@ -4,5 +4,11 @@ APIs:
 
 - Facts Random: https://catfact.ninja/fact
 - Imagen Random: https://cataas.com/cat/says/hello
+    - endpoint para usar: `https://cataas.com/cat/says/${firstThreeWords}?fontSize=50&fontColor=red&json=true`
 
-Recupera un hecho aleatorio de gatos y muestra una imagen de un gato con la primera palabra del hecho recuperado usando la segunda API.
+- Recupera un hecho aleatorio de gatos de la primera API.
+- Recuperar la primera palabra del hecho.
+- Muestra una imagen de un gato con la primera palabra.
+
+- COMPLETADA, 
+- DESAFÍO (VOLVER A REPETIRLA SIN VER EL VIDEO Y CON UNA GRILLA DE GATOS, CON SCROLL ANIMATION Y RESPONSIVE, TODO COMPONETIZADO)
--- proyects/react-prueba-tecnica/src/App.jsx
+++ proyects/react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,46 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
 export function App () {
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (!fact) return
+
+    const threeFirstWords = fact.split(' ', 3).join(' ')
+    console.log(threeFirstWords)
+
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?json=true`)
+      .then(res => res.json())
+      .then(response => {
+        console.log(response)
+        const { _id } = response
+        console.log(_id)
+        const url = `/cat/${_id}/says/${threeFirstWords}`
+        setImageUrl(url)
+      })
+  }, [fact])
+
   return (
-    <h1>App de gatitos</h1>
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`image extracted using the fisrt three words for ${fact}`} />}
+      <button onClick={() => location.reload()}>Más gatitos</button>
+    </main>
   )
 }
