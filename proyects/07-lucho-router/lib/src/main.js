4cca1aed0fc09ea46535677abef7a5e435e5ab9b
luchodpdev <luchodepasquale@gmail.com>
2024-09-18 15:18:17 +0200

    finished

---
+++ proyects/07-lucho-router/lib/src/main.js
@@ -0,0 +1,1 @@
+import{jsx as _jsx}from"react/jsx-runtime";import{StrictMode}from"react";import{createRoot}from"react-dom/client";import App from"./App.jsx";import"./index.css";createRoot(document.getElementById("root")).render(/*#__PURE__*/_jsx(StrictMode,{children:/*#__PURE__*/_jsx(App,{})}));
